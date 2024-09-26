70d1bda657c5d417f1b0486c3f6d5f0433f80f17
percyarrey <tanyitikuarrey@gmail.com>
2024-09-26 13:31:13 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import BentoGridDemo from "@/components/example/bento-grid-demo";
+import BentoGridSecondDemo from "@/components/example/bento-grid-demo-2";
+import BentoGridThirdDemo from "@/components/example/bento-grid-demo-3";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <BentoGridDemo />
+  );
+}
