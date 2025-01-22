e186eff896ef8e9b8569280192cf19d53e6a2ae1
percyarrey <tanyitikuarrey@gmail.com>
2025-01-22 05:09:16 -0800

    seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,12 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Tanyitiku Percy Portfolio",
-  description: "This is a portfolio project created with Next.js",
-  
+  title: "Tanyitiku Percy - Portfolio",
+  description: "Explore the portfolio of Tanyitiku Percy, a skilled developer proficient in Next.js, showcasing projects and solutions.",
+  keywords: "Tanyitiku Percy,Tansolution, portfolio, Next.js, web development, projects, solutions",
+  authors: [{name:"Tanyitiku Percy"},{name:"TanSolution"}],
+  viewport: "width=device-width, initial-scale=1",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -29,6 +32,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="MkAKKGuGMaUOlw1VnnwTY7eBedfxyQOWmn37bmHiu_4" />
+        <meta charSet="UTF-8" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="canonical" href="https://tansolution.vercel.app/" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=YourFontChoice" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
